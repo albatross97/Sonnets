4a7bdbecd09b5a61175805db62a6b82b4d3b6335
albatross97 <ruiwang704@gmail.com>
2021-05-18 12:06:38 -0400

    wait

--- text.js
+++ text.js
@@ -1,16 +1,16 @@
-// var margin_line = {top: 20, right: 150, bottom: 20, left: 40};
-// const poem_svg = d3.select("#poem").append("text")
-//     .attr("width",300)
-//     .attr("height",500)
-//     .attr("transform","translate(" + margin_line.left + "," + margin_line.top + ")");
-// const title = poem_svg.append('text').attr("class","sonnet");
-// const ol = poem_svg.append('ol');
+var margin_line = {top: 20, right: 150, bottom: 20, left: 40};
+const poem_svg = d3.select("#poem").append("text")
+    .attr("width",300)
+    .attr("height",500)
+    .attr("transform","translate(" + margin_line.left + "," + margin_line.top + ")");
+const title = poem_svg.append('text').attr("class","sonnet");
+const ol = poem_svg.append('ol');
 
-// const mycolor = d3.scaleOrdinal()
-//     .domain([1,154])
-//     .range(d3.schemeGnBu[9]);
+const mycolor = d3.scaleOrdinal()
+    .domain([1,154])
+    .range(d3.schemeGnBu[9]);
 
-// function renderPoem(index,id) {
+function renderPoem(index,id) {
 //     title.selectAll('text').remove();
 //     ol.selectAll('li').remove();
 
@@ -38,5 +38,5 @@
 //                 .text(d => `${d.text}`);// add text to each list element
         
 //     })
-// }
-// renderPoem();
+}
+renderPoem();
